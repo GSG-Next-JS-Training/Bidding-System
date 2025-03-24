ed00aa5bdab383ea1e59960f66ec5124909bf6b3
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-24 12:07:06 +0200

    BS-3 : Store token in cookies

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { handlerWrapper } from "@/lib/apiHandler";
 import { loginUser } from "@/modules/repositories/authRepository";
+import { cookies } from "next/headers";
 
 const loginHandler = async (req: NextRequest) => {
   const body = await req.json();
   const { email, password } = body;
   const { token } = await loginUser(email, password);
+  (await cookies()).set('auth-token', token, {
+    httpOnly : true,
+    secure : process.env.NODE_ENV === 'production',
+    maxAge : 3600
+  });
   return NextResponse.json({ message: token }, { status: 200 });
 };
 
