70295d92934729a8326cb2dd86a1082e0effefe5
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-03-26 23:00:09 +0200

    BS-3 : Move html message to separate file

--- utils/auth.ts
+++ utils/auth.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
 import { customAlphabet } from "nanoid";
 import { IUser } from "@/database/user-model";
+import { getEmailHtml } from "@/lib/emailMessage";
 
 const JOSE_SIGNATURE = process.env.JOSE_SIGNATURE || "";
 const SENDER_EMAIL = process.env.SENDER_EMAIL || "";
@@ -32,29 +33,14 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
     secure: false,
   });
   
-  const emailHtml = `
-  <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
-    <h2 style="text-align: center; color: #333;">Password Reset Request</h2>
-    <p style="font-size: 16px; color: #555;">Hello,</p>
-    <p style="font-size: 16px; color: #555;">You recently requested to reset your password. Use the code below to proceed:</p>
-    
-    <div style="text-align: center; margin: 20px 0;">
-      <span style="background-color: #007bff; color: white; padding: 10px 20px; font-size: 24px; font-weight: bold; border-radius: 5px; display: inline-block;">
-        ${html}
-      </span>
-    </div>
-    
-    <p style="font-size: 16px; color: #555;">If you did not request a password reset, please ignore this email.</p>
-    <p style="font-size: 16px; color: #555;">Thank you,</p>
-    <p style="font-size: 16px; font-weight: bold; color: #333;">Bidding System Team</p>
-  </div>`
+
   ;
 
   await transporter.sendMail({
     from: `"Bidding System" <${process.env.SENDER_EMAIL}>`, // sender address
     to,
     subject,
-    html : emailHtml,
+    html : getEmailHtml(html),
   });
 };
 
