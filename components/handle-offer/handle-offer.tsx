4fa39600bebb4391728b3e98476c70cdaa21f95c
yara <yarathabet66@gmail.com>
2025-04-04 01:06:27 +0200

    BS-7: App Drop down list

--- components/handle-offer/handle-offer.tsx
+++ components/handle-offer/handle-offer.tsx
@@ -10,7 +10,7 @@ const HandleOffer = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
       <div className="bg-base-100 p-8 rounded-box shadow-xl max-w-lg w-full">
         <h1 className="text-3xl font-bold text-primary text-center mb-6">
-          Create Company Offer
+          Create Offer Company
         </h1>
         <FormikProvider value={formik}>
           <Form className="space-y-4">
@@ -71,6 +71,23 @@ const HandleOffer = () => {
                 name="service"
                 className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-blue-400 focus:outline-none"
               />
+            </div>
+            <div>
+              <label htmlFor="Owner" className="text-primary font-semibold">
+                Company Owner
+              </label>
+              <select
+                id="Owner"
+                name="owner"
+                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-2  focus:ring-blue-400 focus:outline-none"
+              >
+                <option value="" disabled>
+                  Company Owner
+                </option>
+                <option value="1">Company Owner 1</option>
+                <option value="2">Company Owner 2</option>
+                <option value="3">Company Owner 3</option>
+              </select>
             </div>
             <button
               type="submit"
