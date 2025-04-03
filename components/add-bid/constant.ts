45e254085db7e95b02f27294d186f0e4ed341db9
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-04-03 15:35:39 +0300

    BS-10 : Edit constant file

--- components/add-bid/constant.ts
+++ components/add-bid/constant.ts
@@ -1,6 +1,6 @@
-import { FormValues } from "./types";
+import { AddBiddingRequestBody } from "./api/request.dto";
 
-export const INITIAL_VALUES : FormValues =  {
+export const INITIAL_VALUES : AddBiddingRequestBody =  {
     biddingCompanyId: "",
     bidAmount: 0,
     serviceOffered: "",
