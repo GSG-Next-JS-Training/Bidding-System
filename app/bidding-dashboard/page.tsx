91625edfed6e3bbce695e6fd4e0bdc96dd6250fd
Osama Maree <osamamarei2001@gmail.com>
2025-04-05 14:12:00 +0300

    master: enhance grid page

--- app/bidding-dashboard/page.tsx
+++ app/bidding-dashboard/page.tsx
@@ -3,7 +3,7 @@ import OfferTable from '@/components/offer-table/OfferTable'
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center my-4">
         <div className="p-6 bg-white rounded-lg shadow-lg w-full ">
             <h2 className="text-xl font-semibold text-gray-700 mb-4">Available Offers</h2>
             <OfferTable/>
