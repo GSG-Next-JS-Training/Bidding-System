f522533dc577bd1fa80d78a7c2dc20e209ffc24b
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-04-05 15:40:54 +0300

    BS-16 : Add chat page

---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+import Chat from '@/components/chat/Chat'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-10'>
+      <Chat/>
+    </div>
+  )
+}
+
+export default page
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  images : {
+    remotePatterns : [
+      {
+        protocol : 'https',
+        hostname : '**'
+      }
+    ]
+  },
   // handle CORS for all routes
   async headers() {
     return [
