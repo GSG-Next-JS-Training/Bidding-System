47b827aa05d2b83ea71ea53c1873728677529657
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-04-11 23:46:35 +0300

    BS-21 : Add enhancements

--- components/add-user/api/index.ts
+++ components/add-user/api/index.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { IUser } from "../types";
 import { AddUserResponse } from "./response.dto";
 
-
 export const addUser = async (body: IUser) => {
   const res = await axios.post<AddUserResponse>("/api/user", body);
   return res.data;
